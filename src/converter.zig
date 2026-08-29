33651d9dbc9569901be3ca0e78ddb3f013019d8c
9800 <siiiiiiiib@yahoo.com>
2026-08-30 00:19:05 +0330

    Update converter.zig

--- src/converter.zig
+++ src/converter.zig
@@ -10,40 +10,39 @@ const Pair = struct {
     en: u21,
 };
 
-// نقشه چیدمان استاندارد فارسی ↔ انگلیسی (بر اساس کدپوینت Unicode)
 const pairs = [_]Pair{
-    .{ .fa = 0x0636, .en = 'q' }, // ض
-    .{ .fa = 0x0635, .en = 'w' }, // ص
-    .{ .fa = 0x062B, .en = 'e' }, // ث
-    .{ .fa = 0x0642, .en = 'r' }, // ق
-    .{ .fa = 0x0641, .en = 't' }, // ف
-    .{ .fa = 0x063A, .en = 'y' }, // غ
-    .{ .fa = 0x0639, .en = 'u' }, // ع
-    .{ .fa = 0x0647, .en = 'i' }, // ه
-    .{ .fa = 0x062E, .en = 'o' }, // خ
-    .{ .fa = 0x062D, .en = 'p' }, // ح
-    .{ .fa = 0x062C, .en = '[' }, // ج
-    .{ .fa = 0x0686, .en = ']' }, // چ
-    .{ .fa = 0x067E, .en = '\\' }, // پ
-    .{ .fa = 0x0634, .en = 'a' }, // ش
-    .{ .fa = 0x0633, .en = 's' }, // س
-    .{ .fa = 0x06CC, .en = 'd' }, // ی
-    .{ .fa = 0x0628, .en = 'f' }, // ب
-    .{ .fa = 0x0644, .en = 'g' }, // ل
-    .{ .fa = 0x0627, .en = 'h' }, // ا
-    .{ .fa = 0x062A, .en = 'j' }, // ت
-    .{ .fa = 0x0646, .en = 'k' }, // ن
-    .{ .fa = 0x0645, .en = 'l' }, // م
-    .{ .fa = 0x06A9, .en = ';' }, // ک
-    .{ .fa = 0x06AF, .en = '\'' }, // گ
-    .{ .fa = 0x0638, .en = 'z' }, // ظ
-    .{ .fa = 0x0637, .en = 'x' }, // ط
-    .{ .fa = 0x0632, .en = 'c' }, // ز
-    .{ .fa = 0x0631, .en = 'v' }, // ر
-    .{ .fa = 0x0630, .en = 'b' }, // ذ
-    .{ .fa = 0x062F, .en = 'n' }, // د
-    .{ .fa = 0x0626, .en = 'm' }, // ئ
-    .{ .fa = 0x0648, .en = ',' }, // و
+    .{ .fa = 0x0636, .en = 'q' },
+    .{ .fa = 0x0635, .en = 'w' },
+    .{ .fa = 0x062B, .en = 'e' },
+    .{ .fa = 0x0642, .en = 'r' },
+    .{ .fa = 0x0641, .en = 't' },
+    .{ .fa = 0x063A, .en = 'y' },
+    .{ .fa = 0x0639, .en = 'u' },
+    .{ .fa = 0x0647, .en = 'i' },
+    .{ .fa = 0x062E, .en = 'o' },
+    .{ .fa = 0x062D, .en = 'p' },
+    .{ .fa = 0x062C, .en = '[' },
+    .{ .fa = 0x0686, .en = ']' },
+    .{ .fa = 0x067E, .en = '\\' },
+    .{ .fa = 0x0634, .en = 'a' },
+    .{ .fa = 0x0633, .en = 's' },
+    .{ .fa = 0x06CC, .en = 'd' },
+    .{ .fa = 0x0628, .en = 'f' },
+    .{ .fa = 0x0644, .en = 'g' },
+    .{ .fa = 0x0627, .en = 'h' },
+    .{ .fa = 0x062A, .en = 'j' },
+    .{ .fa = 0x0646, .en = 'k' },
+    .{ .fa = 0x0645, .en = 'l' },
+    .{ .fa = 0x06A9, .en = ';' },
+    .{ .fa = 0x06AF, .en = '\'' },
+    .{ .fa = 0x0638, .en = 'z' },
+    .{ .fa = 0x0637, .en = 'x' },
+    .{ .fa = 0x0632, .en = 'c' },
+    .{ .fa = 0x0631, .en = 'v' },
+    .{ .fa = 0x0630, .en = 'b' },
+    .{ .fa = 0x062F, .en = 'n' },
+    .{ .fa = 0x0626, .en = 'm' },
+    .{ .fa = 0x0648, .en = ',' },
 };
 
 fn lookup(fa_to_en: bool, cp: u21) ?u21 {
@@ -61,6 +60,19 @@ fn appendCodepoint(out: *std.ArrayList(u8), cp: u21) !void {
     var buf: [4]u8 = undefined;
     const n = try std.unicode.utf8Encode(cp, &buf);
     try out.appendSlice(buf[0..n]);
+}
+
+// ✅ تشخیص خودکار زبان متن
+pub fn detectLayout(text: []const u8) KeyboardLayout {
+    var view = std.unicode.Utf8View.init(text) catch return .english;
+    var it = view.iterator();
+    while (it.nextCodepoint()) |cp| {
+        // محدوده حروف عربی/فارسی + حروف فارسی خاص
+        if ((cp >= 0x0600 and cp <= 0x06FF) or (cp >= 0xFB50 and cp <= 0xFEFF)) {
+            return .persian;
+        }
+    }
+    return .english;
 }
 
 pub fn convertText(text: []const u8, from: KeyboardLayout, to: KeyboardLayout, allocator: std.mem.Allocator) ![]u8 {
