5d356279599afa83b0cd7f365581be3a25de6773
9800 <siiiiiiiib@yahoo.com>
2026-08-29 23:21:51 +0330

    Create build.zig

---
+++ build.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{
+        .default_target = .{
+            .os_tag = .windows,
+            .cpu_arch = .x86_64,
+        },
+    });
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "LangReplace",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkSystemLibrary("user32");
+    exe.linkSystemLibrary("shell32");
+    exe.linkSystemLibrary("gdi32");
+    exe.linkSystemLibrary("kernel32");
+    exe.linkSystemLibrary("ole32");
+    exe.linkSystemLibrary("advapi32");
+    exe.linkSystemLibrary("winhttp");
+
+    exe.subsystem = .Windows;
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
